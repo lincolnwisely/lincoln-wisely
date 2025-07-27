8592722aad3f1b17dce2e04507fc8f014e70ed36
Lincoln Wisely <lincolnwisely@gmail.com>
2025-07-27 18:52:56 -0500

    fix analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="G-V8N45PYTJE" />
+            <GoogleAnalytics gaId="G-V8N45PYTJE" />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
