0747d93accba69dccbe55bbe137ea9c362ef9baa
Lincoln Wisely <lincoln.wisely@plexapp.com>
2025-09-11 11:41:53 -0500

    add GTM scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleAnalytics gaId="G-V8N45PYTJE" />
-      <Analytics />
+      <head>
+        <GoogleAnalytics gaId="G-V8N45PYTJE" />
+        <Analytics />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KR7QSDZX');`,
+          }}
+        />
+      </head>
       <body className={`${geistSans.variable} ${dmMono.variable} antialiased`}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KR7QSDZX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         {children}
       </body>
     </html>
