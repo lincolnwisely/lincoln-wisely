b50e8eb373852c76734a65522bf9ca226e0f3d33
Lincoln Wisely <lincolnwisely@gmail.com>
2025-10-03 14:18:05 -0500

    create new ProjectCard, add flutter project

--- src/app/fixtures/types.tsx
+++ src/app/fixtures/types.tsx
@@ -2,3 +2,9 @@ export interface Chip {
     url: string;
     label: string;
 }
+
+export interface Link {
+    url: string;
+    label: string;
+    image?: string;
+}
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,14 +1,32 @@
-import HomeVideos from './home-videos';
-import SharonWisely from './sharon-wisely';
+
+import ProjectCard from './ProjectCard'
+import vhs_chips from '../app/fixtures/vhs_chips.json'
+import mern_chips from '../app/fixtures/mern_chips.json'
+import flutter_chips from '../app/fixtures/flutter_chips.json'
 
 export default function Projects() {
   return (
-    <div className="flex flex-col lg:flex-row items-stretch">
-      <HomeVideos />
-      <div className="block lg:hidden">
-        <br />
+    <div className="space-y-8">
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <ProjectCard
+          title="Digitizing Home Videos"
+          description="As the self-proclaimed Family Historian, I have embarked on the
+            greatly rewarding task of digitizing and hosting our
+            home videos."
+          data={vhs_chips.chips} />
+
+
+        <ProjectCard
+          title="Sharon Wisely Jewelry"
+          description="Features user authentication w/ JWT tokens, product management and Stripe payments integration."
+          data={mern_chips.chips}
+          links={[{ url: "https://github.com/lincolnwisely/sw-jewelry", image: "/img/github.png", label: "source" }]} />
+
+        <ProjectCard
+          title="Flutter App"
+          description="Leveraging AI models for image analysis and facial recognition to solve the most critical first-world problems. Project for learning only."
+          data={flutter_chips.chips} />
       </div>
-      <SharonWisely />
     </div>
   );
 }
